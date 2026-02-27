9ee3dab22694e06606ba602b1615a572d7111b90
Vitor Justo <vitor.justo@inovafarma.com.br>
2026-02-27 17:07:00 -0300

    start picross code

--- src/Components/UI/GameContent/GameContent.tsx
+++ src/Components/UI/GameContent/GameContent.tsx
@@ -1,10 +1,12 @@
 import '../../../index.css'
+import Picross from '../../Games/Picross/Picross'
 import './GameContent.css'
 
 export default function GameContent()
 {
   return (
 	<div className="GameContent">
+    <Picross/>
 	</div>
   )
 }
--- src/Entites/Game.ts
+++ src/Entites/Game.ts
@@ -10,5 +10,4 @@ export default class Game
 		this.Name = name;
 		this.GameModes = gameModes;
 	}
-
 }
