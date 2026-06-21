a7d09f85c304564090d1eb6de95bc0ce32f892e2
vitorjusto <vitorhugojusto1@gmail.com>
2026-06-21 14:56:45 -0300

    feat: create sudoku

--- src/Components/UI/GameContent/GameContent.tsx
+++ src/Components/UI/GameContent/GameContent.tsx
@@ -1,12 +1,15 @@
 import '../../../index.css'
 import Picross from '../../Games/Picross/Picross'
+import Sudoku from '../../Games/Sudoku/Sudoku'
+import GameContentFooter from '../GameContentFooter/GameContentFooter'
 import './GameContent.css'
 
 export default function GameContent()
 {
   return (
 	<div className="GameContent">
-    <Picross/>
+    	<Sudoku/>
+		<GameContentFooter/>
 	</div>
   )
 }
